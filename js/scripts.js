fc062e1ac0fed3c4fa876d8ed25b5949a40ccff2
Michael Larragueta and Marina Poltorak <michael@>
2019-03-25 14:29:33 -0700

    Add everything

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function(){
+  $("h1").click(function(){
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
