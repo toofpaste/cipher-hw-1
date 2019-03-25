0258acbda5cd69bf4fc47e8dbced332a9aa645a2
Michael Larragueta and Marina Poltorak <michael@>
2019-03-25 15:18:44 -0700

    write functions to split, join, reverse and capitalize

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,45 @@
 var getSent = function(question){
   var sent = prompt(question);
-  var length = sent.length;
+  getLength(sent);
+};
+
+var getLength = function(sent){
+    var length = sent.length;
+    capitlize(length, sent);
+};
+
+var capitlize = function(length, sent){
   var last = sent.charAt(length - 1);
   var first = sent.charAt(0);
   last = last.toUpperCase();
   first = first.toUpperCase();
-  sent = sent.slice(1);
-  sent = sent.split("");
-  sent = sent.reverse();
-  sent = sent.join("");
-  sent = sent.slice(1);
-  sent = sent.split("");
-  sent = sent.reverse();
-  sent = sent.join("");
-  alert(first + sent + last);
-  return sent;
+  split(first, last, sent);
 };
 
-var save = getSent("Enter a sentence to capitalize the first and last letter: ");
+var split = function(first, last, sent){
+  sent = sent.slice(1);
+  sent = sent.split("");
+  sent = sent.reverse();
+  sent = sent.join("");
+  sent = sent.slice(1);
+  sent = sent.split("");
+  sent = sent.reverse();
+  sent = sent.join("");
+  printNorm(first, last, sent);
+};
+
+var printNorm = function(first, last, sent){
+  var capitNorm = first + sent + last;
+    alert(capitNorm);
+    printBack(first, last, sent);
+};
+
+var printBack = function(first, last, sent){
+
+  var capitBack = last + sent + first;
+
+  alert(capitBack);
+};
+
+
+getSent("Enter a sentence to capitalize the first and last letter: ");
