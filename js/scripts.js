d512f5d07cad808cd0be208e6b729ebd13fc5726
Michael Larragueta and Marina Poltorak <marina@>
2019-03-25 14:58:11 -0700

    Add script to capitalize first and last letter of a string

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
-$(function(){
-  $("h1").click(function(){
-    alert("This is a header.");
-  });
+var getSent = function(question){
+  var sent = prompt(question);
+  var length = sent.length;
+  var last = sent.charAt(length - 1);
+  var first = sent.charAt(0);
+  last = last.toUpperCase();
+  first = first.toUpperCase();
+  sent = sent.slice(1);
+  sent = sent.split("");
+  sent = sent.reverse();
+  sent = sent.join("");
+  sent = sent.slice(1);
+  sent = sent.split("");
+  sent = sent.reverse();
+  sent = sent.join("");
+  alert(first + sent + last);
+  return sent;
+};
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-});
+var save = getSent("Enter a sentence to capitalize the first and last letter: ");
